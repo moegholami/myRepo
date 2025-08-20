197c9590d73a8453039c481221a21224983f38d2
Mohammad Gholami <92236088+moegholami@users.noreply.github.com>
2025-08-19 23:07:23 -0400

    third commit - deleted git commands

--- note.ps1
+++ note.ps1
@@ -1,28 +1,3 @@
-Change 1
-
-
-
-
-
-git --version
-git init
-git clone src dst
-git clone src
-
-Global configurations
-who am I?
-git config --global --list
-git config --global user.name "Mohammad Gholami"
-git config --global user.email gholamy@gmail.com
-
-git config --list --show-origin      #see where coming from :q
-git config --list
-
-git config --global init.defaultBranch main #this is very common to use instead of master
-
-#Pay attention to the current .git folder content, especially the objects folder
-git add .
-
 new LINE
 
 New LINE 2
